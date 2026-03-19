e6e0e99467e8b966cb7ca5ee2b746c8d622c7ade
happinesschildren120-sketch <happinesschildren120@gmail.com>
2026-03-20 01:17:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const mineflayer = require('mineflayer');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('AFK Bot is Running!'));
+app.listen(3000, () => console.log('Web Server Ready'));
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'YOUR_ATERNOS_IP', // Ekhane Aternos IP dao
+        port: 19132,             // Bedrock-er default port eita thake
+        username: 'AFK_Bot',
+        version: false,          // Auto detect
+        auth: 'offline'          // Aternos-er cracked server-er jonno
+    });
+
+    bot.on('spawn', () => {
+        console.log('Bot joined the Bedrock server!');
+        setInterval(() => {
+            bot.look(bot.entity.yaw + 0.1, 0);
+        }, 5000);
+    });
+
+    bot.on('end', () => {
+        console.log('Disconnected. Reconnecting...');
+        setTimeout(createBot, 5000);
+    });
+
+    bot.on('error', (err) => console.log(err));
+}
+
+createBot();
